eb13418149f429ba49beac5b74f0cb23dd43335b
Jing-Long <41318244+Jing-Long@users.noreply.github.com>
2018-07-30 09:58:05 +1000

    Create COMP6730_notes.py

---
+++ COMP6730_notes.py
@@ -0,0 +1,6 @@
+Lecture 2.
+get the current work directory: 
+import os 
+os.getcwd( )
+
+whitespace: spaces, tab, end-of-line are whitespace and they are used to show the end of a statement or indentation. At other times, whitespace is ignored.
