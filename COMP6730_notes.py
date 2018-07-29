7271bc4caf1eb102e1c2a1dd39df9ef29ea6d031
Jing-Long <41318244+Jing-Long@users.noreply.github.com>
2018-07-30 09:59:10 +1000

    Update COMP6730_notes.py

--- COMP6730_notes.py
+++ COMP6730_notes.py
@@ -3,4 +3,5 @@ get the current work directory:
 import os 
 os.getcwd( )
 
-whitespace: spaces, tab, end-of-line are whitespace and they are used to show the end of a statement or indentation. At other times, whitespace is ignored.
+whitespace: spaces, tab, end-of-line are whitespace and they are used to show the end of a statement or indentation.
+            At other times, whitespace is ignored.
