009068efe5ebc340813bad09dfebdb7c7555ac02
Jing-Long <41318244+Jing-Long@users.noreply.github.com>
2018-08-06 11:59:37 +1000

    Update thinkpython_notes.py

--- thinkpython_notes.py
+++ thinkpython_notes.py
@@ -1,35 +1,12 @@
 Ch1.
 
-In python 3, print (sth)
-In python 2, print sth
+
 
 Ch2.
-Variable name cannot begin with a number.
-Variable name cannot use Python's keywords: 
-False class finally is return
-None continue for lambda try
-True def from nonlocal while
-and del global not with
-as elif if or yield
-assert else import pass
-break except in raise
+
 
 Ch3. Functions
 
 
-log is ln
-log10 is lg
 
-Function names can be letters, numbers and underscore but the first character cannot me a number. 
-Function names cannot be the python's keywords. Avoid to define function and variable with the same name.
-Function names are case sensitive.
 
-Single and double quotes are the same in python, double quotes are useful when there is an apostrophe in the string. 
-Straight quotes ' ' " " are legal in python
-Curly quotes  ‘ ’ “ ” are not. 
-
-When define the function: def function_name(parameter):
-When call the function: function_name(argument)
-The name of the argument has nothing to do with the name of the parameter. 
-
-Parameters and variables that created in a function are local.
