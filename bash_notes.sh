d547336f9f74b6f73e4215289d69e3c2531c129a
Jing-Long <41318244+Jing-Long@users.noreply.github.com>
2019-08-02 14:34:44 +1000

    Update bash_notes.sh

--- bash_notes.sh
+++ bash_notes.sh
@@ -1,10 +1,35 @@
 % is just used as a comment symbol in this note, it has nothing to do with bash syntax!
+
+man [command] % bring up the manual page of a program/command to learn about 
+                possible command-line options for shell program(s)
+
 echo $SHELL % gives /bin/bash, it means the file bash is the Bash program that starts a Bash shell.
+~ % a shortcut to access home directory, can use echo $HOME to find the home directory
+.file_name % means this file is hidden. 
+ls -a % can list all files and directories, including hidden ones.
+
+add PS1=’\u@\h:\w>’ to .bashrc % change the Bash console prompt style to username@hostname:current_directory>
+add test -s ~/.alias && . ~/.alias to .bashrc % check whether file .alias exists, if so, run it. This step ensures that
+                                                the content of .alias is read and added to the Bash environment. 
+add in .alias: alias ls='ls --color=auto $OPTIONS' % enable colourised output according to their types, eg. file, folder,etc.
+               alias ll='ls -colorlh $OPTIONS' % defines a new command alias called ll, which calls ls with the additional 
+                                                 options -l for long-listing (more details) 
+                                                 and -h for human-readable file size output.
+ls -l % gives a list which begins with file type and permissions.
+        -rwxrwxrwx % means this is a file, Owner, Group and Other can read(r), write(w) and execute(x) it.
+        drwxr--r-- % means this is a directory, Group and Other can only read(r) but not write(-) or execute(-) it. 
+     
+source .bashrc or restart Bash or open a new Bash shell to activate changes to .bashrc and .alias
+
 chmod +x file_name  % make this file executable
 rsync % this is a program to copy and synchronise files between computers
 gnuplot % this is a plotting package
-man [command] % bring up the manual page of a program/command to learn about 
-                possible command-line options for shell program(s)
+ssh % connect to another computer.
+ssh -X or -Y % forward graphical windows from the connected computer to my local machine through the SSH connection.      
+
+
+
+
 
 Files start with #! (called hashbang) in first line and have been made executable can be run with ./file_name directly.
 
